666f247ec7ffc1bb40688d799a1081e6dd47a0cd
Eurybus <wraitholyth@gmail.com>
2016-09-14 19:35:22 +0300

    Completed Ikkunalaskuri

---
+++ IkkunaLaskuriProper/Index.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="IkkunaLaskuriProper.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #fieldWidth, #fieldHeight, #fieldFrameWidth {
+            width: 48px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <p>Leveys:  <asp:TextBox id="fieldWidth" runat="server"></asp:TextBox>mm<br />
+        Kokeus: <asp:TextBox id="fieldHeight" runat="server"></asp:TextBox>mm<br />
+        Karmipuun leveys(W): <asp:TextBox id="fieldFrameWidth" runat="server"></asp:TextBox>mm
+        </p>
+        <asp:Button runat="server" ID="btnSubmit" Text="Laske tarjoushinta" OnClick="btnSubmit_Click" />
+        <p>
+            Ikkunan Pinta-Ala: <asp:Label ID="lblArea" runat="server">N/A</asp:Label>m2<br />
+            Karmin piiri: <asp:Label ID="lblPerimeter" runat="server">N/A</asp:Label>m<br />
+            Tarjoushinta: <asp:Label ID="lblPrice" runat="server">N/A</asp:Label><br />
+        </p>
+    </div>
+    </form>
+</body>
+</html>
---
+++ IkkunaLaskuriProper/Index.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Configuration; //Web.config lukemista varten
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IkkunaLaskuriProper
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Set the default values to fields
+            /*fieldFrameWidth.Text = ConfigurationManager.AppSettings["defaultFrameWidth"];
+            fieldHeight.Text = ConfigurationManager.AppSettings["defaultHeight"];
+            fieldWidth.Text = ConfigurationManager.AppSettings["defaultWidth"];*/
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double resultPer, resultArea, resultBzlArea;
+
+                double width = double.Parse(fieldWidth.Text) / 1000;
+                double height = double.Parse(fieldHeight.Text) / 1000;
+                double bzlWidth = double.Parse(fieldFrameWidth.Text);
+
+                resultBzlArea = BezelCalculatePerimerer(bzlWidth, width, height);
+                resultPer = calculatePerimeter(width, height);
+                resultArea = calculateArea(width, height);
+
+                updateValues(resultBzlArea, resultPer, resultArea, calculatePrice(resultArea, resultPer));
+            }
+            catch (Exception ex)
+            {
+                lblPrice.Text = ex.Message + "Stacktrace: " + ex.StackTrace;
+            }
+        }
+        private double calculateArea(double width, double height)
+        {
+            return width * height;
+        }
+        private double calculatePerimeter(double width, double height)
+        {
+            return width * 2 + height * 2;
+        }
+        public static double BezelCalculatePerimerer(double bzlwidth, double xLength, double yLength)
+        {
+            return ((bzlwidth * xLength) * 2) + ((bzlwidth * yLength) * 2);
+        }
+        private void updateValues(double bzlArea, double perimeter, double area, double price)
+        {
+            lblArea.Text = area.ToString();
+            lblPerimeter.Text = perimeter.ToString();
+            lblPrice.Text = price.ToString();
+        }
+        private double calculatePrice(double area, double bzlPer)
+        {
+            double margin = double.Parse(ConfigurationManager.AppSettings["salesMargin"]) / 10;
+            double glassSqPrice = double.Parse(ConfigurationManager.AppSettings["glassSqPrice"]);
+            double workPrice = double.Parse(ConfigurationManager.AppSettings["workPrice"]);
+            double aluLineMeterPrice = double.Parse(ConfigurationManager.AppSettings["aluminiumLinealMeter"]);
+
+            return (1 + margin) * ((area * glassSqPrice) + (bzlPer * aluLineMeterPrice) + workPrice);
+        }
+    }
+}

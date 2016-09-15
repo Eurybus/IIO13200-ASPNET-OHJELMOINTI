f92f4e99c74eb9aa12d6ca77b4912fcad12304e1
Eurybus <H8535@MEDI-426-WS-18>
2016-09-15 11:14:32 +0300

    Fucked around with Razer

---
+++ Saitti/Oppilaat.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Oppilaat.aspx.cs" Inherits="Oppilaat" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <h2>Oppilaat</h2>
+        <div>
+            <asp:Button ID="btnGet3" Text="Hae testioppilaat" OnClick="btnGet3_Click" runat="server"/>
+            <asp:Button ID="btnGetReal" Text="Hae oikea data" OnClick   ="btnGetReal_Click" runat="server" />
+        </div>
+        <div id="tulos">
+            <asp:GridView ID="gvStudents" runat="server" />
+        </div>
+        <div id="footer">
+            <asp:Label ID="lblMessages" runat="server" />
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Saitti/Oppilaat.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Oppilaat : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnGet3_Click(object sender, EventArgs e)
+    {
+        DataTable dt = JAMK.ICT.Data.DBPlacebo.Get3TestStudents();
+        gvStudents.DataSource = dt;
+        gvStudents.DataBind();
+    }
+
+    protected void btnGetReal_Click(object sender, EventArgs e)
+    {
+        string cs = ConfigurationManager.ConnectionStrings["Oppilaat"].ConnectionString;
+        //haetaan oppilaat tietokannasta DataTableen
+        string message = ""; //this is needed for next function
+        try
+        {
+            DataTable dt = JAMK.ICT.Data.DBPlacebo.GetAllStudentsFromSQLServer(cs,"oppilaat", out message);
+            gvStudents.DataSource = dt;
+            gvStudents.DataBind();
+            lblMessages.Text = message;
+        }
+        catch (Exception ex)
+        {
+            lblMessages.Text = ex.Message;
+        }
+    }
+}
